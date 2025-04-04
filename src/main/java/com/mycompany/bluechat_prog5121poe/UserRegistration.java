24c13242a0e932fed2176fa047f1ac9e259773e3
st10474559 <st10474559@rcconnect.edu.za>
2025-04-04 15:02:11 +0200

    First Commit

---
+++ src/main/java/com/mycompany/bluechat_prog5121poe/UserRegistration.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bluechat_prog5121poe;
+
+/**
+ *
+ * @author manyi
+ */
+public class UserRegistration {
+    
+}
